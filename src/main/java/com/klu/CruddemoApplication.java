ef52469b0edb652e197629f704debd21112f1c67
MVaiswasmitha <2300030396@kluniversity.in>
2025-08-13 12:12:58 +0530

    Update CruddemoApplication.java

--- src/main/java/com/klu/CruddemoApplication.java
+++ src/main/java/com/klu/CruddemoApplication.java
@@ -1,19 +1,12 @@
-package com.klu.cruddemo;
+package com.klu;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class CrudDemoApplication extends SpringBootServletInitializer {
+public class CruddemoApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(CrudDemoApplication.class, args);
-    }
-
-    @Override
-    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
-        return builder.sources(CrudDemoApplication.class);
+        SpringApplication.run(CruddemoApplication.class, args);
     }
 }
